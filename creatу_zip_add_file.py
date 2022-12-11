5625e71746095c472ece740f241e3478db00a537
annavishnyakova <vishnyakova0307@gmail.com>
2022-12-11 21:15:09 +0300

    дз готово к проверке

--- creatу_zip_add_file.py
+++ creatу_zip_add_file.py
@@ -1,5 +1,7 @@
 import zipfile, os
 from os.path import basename
+from openpyxl import load_workbook
+import csv
 
 #путь к папкам
 path_inputfiles = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'files')
@@ -17,9 +19,13 @@ with zipfile.ZipFile(path_zip, mode='w',
         zf.write(add_file, basename(add_file))
 
 # Проверка содержимого xlsx
-with zipfile.ZipFile(path_zip, mode='r') as zip_file:
-        text = str(zip_file.read('file_xlsx.xlsx'))
-        assert text.__contains__("February")
+with zipfile.ZipFile(path_zip) as zf:
+    xf = zf.extract("file_xlsx.xlsx")
+    xlsxfile = load_workbook(xf)
+    sheet = xlsxfile.active
+    assert sheet.cell(row=7, column=1).value == "February", f"Expected result: {'February'}, " \
+                                                           f"actual result: {sheet.cell(row=7, column=1).value}"
+    os.remove("file_xlsx.xlsx")
 
 # Проверка содержимого pdf
 with zipfile.ZipFile(path_zip, mode='r') as zip_file:
@@ -27,7 +33,17 @@ with zipfile.ZipFile(path_zip, mode='r') as zip_file:
     assert text.__contains__('PDF')
 
 # Проверка содержимого csv
-with zipfile.ZipFile(path_zip, mode='r') as zip_file:
-    text = str(zip_file.read('file_csv.csv'))
-    assert text.__contains__('Anna,Pavel,Peter')
+with zipfile.ZipFile(path_zip) as zf:
+    cf = zf.extract("file_csv.csv")
+    with open(cf) as csvfile:
+        csvfile = csv.reader(csvfile)
+        list_csv = []
+        for r in csvfile:
+            text = "".join(r).replace(";", " ")
+            list_csv.append(text)
+
+        assert list_csv[
+                   0] == "Anna Pavel Peter", f"Expected result: {'Anna Pavel Peter'}, " \
+                                                        f"actual result: {list_csv[0]}"
+        os.remove("file_csv.csv")
 
