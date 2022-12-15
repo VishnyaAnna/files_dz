8eceaa0b18ca11950b24b27d530245d6946c9405
annavishnyakova <vishnyakova0307@gmail.com>
2022-12-15 20:22:38 +0300

    дз поправила

--- test_creatу_zip_add_file.py
+++ test_creatу_zip_add_file.py
@@ -3,10 +3,9 @@ from os.path import basename
 from openpyxl import load_workbook
 import csv
 
-#путь к папкам
+# путь к папкам
 path_inputfiles = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'files')
 path_outputfiles = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources')
-
 
 files_dir = os.listdir(path_inputfiles)  # список имен фалов в папке "files"
 path_zip = os.path.join(path_outputfiles, "test.zip")  # путь к архиву
@@ -18,19 +17,26 @@ with zipfile.ZipFile(path_zip, mode='w',
         add_file = os.path.join(path_inputfiles, file)
         zf.write(add_file, basename(add_file))
 
+
 # Проверка содержимого xlsx
-with zipfile.ZipFile(path_zip) as zf:
-    xf = zf.extract("file_xlsx.xlsx")
-    xlsxfile = load_workbook(xf)
-    sheet = xlsxfile.active
-    assert sheet.cell(row=7, column=1).value == "February", f"Expected result: {'February'}, " \
-                                                           f"actual result: {sheet.cell(row=7, column=1).value}"
+def check_xlsx_file():
+    with zipfile.ZipFile(path_zip) as zf:
+        xf = zf.extract("file_xlsx.xlsx")
+        xlsxfile = load_workbook(xf)
+        sheet = xlsxfile.active
+        assert sheet.cell(row=7, column=1).value == "February", f"Expected result: {'February'}, " \
+                                                                f"actual result: {sheet.cell(row=7, column=1).value}"
     os.remove("file_xlsx.xlsx")
 
+
 # Проверка содержимого pdf
-with zipfile.ZipFile(path_zip, mode='r') as zip_file:
-    text = str(zip_file.read('file_pdf.pdf'))
-    assert text.__contains__('PDF')
+def check_pdf_file():
+    with zipfile.ZipFile(path_zip, mode='r') as zip_file:
+        text = str(zip_file.read('file_pdf.pdf'))
+        assert text.__contains__('PDF')
+
+    os.remove("file_pdf.xlsx")
+
 
 # Проверка содержимого csv
 with zipfile.ZipFile(path_zip) as zf:
@@ -44,6 +50,5 @@ with zipfile.ZipFile(path_zip) as zf:
 
         assert list_csv[
                    0] == "Anna Pavel Peter", f"Expected result: {'Anna Pavel Peter'}, " \
-                                                        f"actual result: {list_csv[0]}"
-        os.remove("file_csv.csv")
-
+                                             f"actual result: {list_csv[0]}"
+os.remove("file_xlsx.xlsx")
